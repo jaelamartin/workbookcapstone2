22570538e2af0e8797e529b1d00b0e7f95cd35fb
Jaela Martin <jmartin@appdev.yearup.org>
2023-11-20 13:04:24 -0500

    final work.2

--- enjoy-the-outdoors/scripts/mountainData.js
+++ enjoy-the-outdoors/scripts/mountainData.js
@@ -4,7 +4,7 @@ const mountainsArray = [
         elevation: 6288,
         effort: "Strenuous",
         img: "Washington-StoryImage_2.jpg",
-        desc: "Mt. Washington (6,288 feet) is the highest peak east of the Mississippi River and north of the Carolinas. The upper part of the mountain has a climate similar to that of northern Labrador and supports a variety of alpine flora and fauna.",
+        desc: "Mt. Washington (6,288 feet) is the highest peak east of the Mississippi River and north of the Carolinas.The upper part of the mountain has a climate similar to that of northern Labrador and supports a variety of alpine flora and fauna.",
         coords: {
             lat: 44.270403,
             lng: -71.303501
